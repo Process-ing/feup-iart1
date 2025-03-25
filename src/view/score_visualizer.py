a33923fdbc5c7e3aba2f156a451d8245f6ba5990
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2025-03-25 17:22:17 +0000

    feat: added line representing the max score

--- src/view/score_visualizer.py
+++ src/view/score_visualizer.py
@@ -6,15 +6,28 @@ matplotlib.use('TkAgg')
 class ScoreVisualizer:
     def __init__(self):
         self.scores = []
+        self.max_scores = []
         self.fig, self.ax = plt.subplots()
-        self.line, = self.ax.plot([], [], 'r-')
+        self.line, = self.ax.plot([], [], 'r-', label='Score')
+        self.max_line, = self.ax.plot([], [], 'b-', label='Max Score')
+        self.ax.legend()
 
     def update_scores(self, new_score):
+        if (self.scores and new_score == self.scores[-1]):
+            return
+
+        if (not self.max_scores or new_score > self.max_scores[-1]):
+            self.max_scores.append(new_score)
+        else:
+            self.max_scores.append(self.max_scores[-1])
+
+
         self.scores.append(new_score)
         self.update_plot()
 
     def update_plot(self):
         self.line.set_data(range(len(self.scores)), self.scores)
+        self.max_line.set_data(range(len(self.max_scores)), self.max_scores)
         self.ax.relim()
         self.ax.autoscale_view()
         self.fig.canvas.draw()
