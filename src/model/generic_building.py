1ee4e6751af1170bb3e195fc51a2a2c6a15716cf
Rodrigo Coelho <racoelhosilva@gmail.com>
2025-04-06 10:23:50 +0100

    docs: finish generic building documentation

--- src/model/generic_building.py
+++ src/model/generic_building.py
@@ -1,15 +1,38 @@
 from abc import abstractmethod
 
-
 class GenericBuilding:
+    '''
+    Abstract class representing a generic building.
+
+    This class defines the basic structure and methods that any building
+    implementation should follow.
+    '''
     @abstractmethod
     def get_num_routers(self) -> int:
+        '''
+        Get the number of routers in the building.
+        
+        Returns:
+            int: The number of routers in the building.
+        '''
         pass
 
     @abstractmethod
     def get_num_connected_cells(self) -> int:
+        '''
+        Get the number of connected cells in the building.
+
+        Returns:
+            int: The number of connected cells in the building.
+        '''
         pass
 
     @abstractmethod
     def get_coverage(self) -> int:
+        '''
+        Get the coverage of the building.
+
+        Returns:
+            int: The coverage of the building.
+        '''
         pass
